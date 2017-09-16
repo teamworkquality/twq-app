3c33f418b992a60fd6cce4b84987b4abf9ed0951
fbormann <felipe.bormann@gmail.com>
2017-09-15 21:46:26 -0300

    feature(user-endpoints): added endpoints and their respective tests
    add user/ (create)
    add user/{id} (delete)
    add user/{id} (get)

--- api/users/models.py
+++ api/users/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 class User(AbstractBaseUser):
-    full_name = models.CharField(max_length=250)
-    email = models.EmailField()
-    is_admin = models.BooleanField(default=False)
-    password = models.CharField(max_length=250)
+    full_name = models.CharField(max_length=250, blank=False)
+    email = models.EmailField(unique=True)
+    is_admin = models.BooleanField(default=False, blank=False)
+
+    USERNAME_FIELD = "email"
 
 class Employee(User):
     pass
