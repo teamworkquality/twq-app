e633ea43a2010bccf016002cc7c4147b46c9f9e3
Renato Vieira Leite de Barros <renato.barros@ati.pe.gov.br>
2017-09-19 16:30:58 -0300

    Iniciando vue

---
+++ web/twq-front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div>Navbar aqui</div>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import '~bulma/css/bulma.css';
+</style>
---
+++ web/twq-front/src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="columns">
+    <div class="column is-half">
+      <div>Bem-vindo!</div>
+      <div>
+        <form v-on:submit.prevent="onSubmit()" class="columns">
+          <div class="column">
+            <div class="field">
+              <div class="control">
+                <input class="input" type="text" placeholder="E-mail" v-model="email">
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="field">
+              <div class="control">
+                <input class="input" type="text" placeholder="Senha" v-model="password">
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="control">
+              <button class="button is-dark is-fullwidth">Entrar</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  methods: {
+    onSubmit() {
+      console.log(this.email);
+      console.log(this.password);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
