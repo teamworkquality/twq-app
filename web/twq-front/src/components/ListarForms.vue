e7d0eb69bc5ec4489037f8a20d978bc569800080
Bárbara Lima <Brbara Lima>
2017-09-24 01:13:55 -0300

    Alterando a lista

--- web/twq-front/src/components/ListarForms.vue
+++ web/twq-front/src/components/ListarForms.vue
@@ -7,49 +7,27 @@
 			<table class="table has-vmargin-50 is-bordered is-stripe is-fullwidth">
 				<thead>
 					<tr>
-						<th>Name</th>
-						<th>Contact</th>
-						<th>Country</th>
+						<th>Nome</th>
+						<th>Data</th>
+						<th>Permissão</th>
 					</tr>
 				</thead>
 
-				<tbody>
+				<tbody v-for = "item in formularios">
 					<tr>
-						<td>Claudemira Florbela Anjos</td>
-						<td>Nazaré Luciana Godinho</td>
+						<td>{{item.nome}}</td>
+						<td>{{item.data}}</td>
 						<td>Brasil</td>
 					</tr>
-					<tr>
-						<td>Amadeu Marcelo Coimbra</td>
-						<td>Floriano Almerinda Aragão</td>
-						<td>Portugal</td>
-					</tr>
-					<tr>
-						<td>Eva Viridiana Alencar</td>
-						<td>Constantino Adérito Carromeu</td>
-						<td>Angola</td>
-					</tr>
-					<tr class="is-selected">
-						<td>Godofredo Ulrico Neves</td>
-						<td>Clarindo Teodorico Paixão</td>
-						<td>Alemanha</td>
-					</tr>
-					<tr>
-						<td>Alfredo Lucrécia Amaro</td>
-						<td>Gueda Sancha Fonseca</td>
-						<td>Argentina</td>
-					</tr>
-					<tr>
-						<td>Ema Collin Nazário</td>
-						<td>Neuza Dirceu Veleda</td>
-						<td>Uruguai</td>
-					</tr>
+					
 				</tbody>
 			</table>
         <div class="control">
 				<div class="columns">
 					<div class="column is-half">
+					     <router-link :to="{name: 'CriarForm'}">
 							<button class="button is-dark is-medium">Novo</button>
+					     </router-link>
 					</div>
 					<div class="column">
 						<button class="button is-light is-medium">Editar</button>
@@ -64,7 +42,11 @@
 
 <script>
 export default {
-	name: 'listarForms'
+	name: 'listarForms',
+	data: () =>({
+		formularios:[{nome:'Formulário 1',data:'10/10/2017'},{nome:'Formulário 2',data:'20/10/2017'}]
+	})
+
 }
 </script>
 <style scoped></style>
