3ed582cdc25ce647ee0423f1ed5489464ac94125
Bárbara Lima <Brbara Lima>
2017-09-24 01:15:11 -0300

    Atualizando a tela de criar formulário

--- web/twq-front/src/components/CriarForm.vue
+++ web/twq-front/src/components/CriarForm.vue
@@ -1,45 +1,25 @@
 <template>
-	<div class = "container">
+<div class = "container">
 		<div class="columns is-multiline is-mobile">
 			<div class="field - column is-one-quarter">
 			  <label class="label">Nome</label>
 			  <div class="control">
-	    		<input class="input " type="text" placeholder="Nome do formulário">
+	    		<input class="input " type="text" placeholder="Nome do formulário" v-model ="nome_formulario">
 	          </div>
 			</div>
 			<div class="field - column is-one-quarter">
 			  <label class="label">Data de Envio</label>
 			  <div class="control">
-	    		<input class="input " type="text" placeholder="DD/MM/AAAA">
+	    		<input class="input " type="text" placeholder="DD/MM/AAAA" v-model ="data_envio">
 	          </div>
 			</div>
-			<div class= "field columns is-multiline is-mobile is-narrow">
-			  <div class="control">
-			    <label class="radio is-one-quarter">
-			      <input type="radio" name="question" checked="true">
-			      Escolha Única
-			    </label>
-			    <label class="radio is-one-quarter">
-			      <input type="radio" name="question">
-			      Númerico
-			    </label>
-			    <label class="radio is-one-quarter">
-			      <input type="radio" name="question" checked="true">
-			      Múltipla escolha
-			    </label>
-			    <label class="radio is-one-quarter">
-			      <input type="radio" name="question" checked="true">
-			      Texto Aberto
-			    </label>
-			  </div>
-			</div>
+
 			<div class="field - column is-one-quarter">
 			  <label class="label">Equipe</label>
 			  <div class="control">
 			    <div class="select">
-			      <select>
+			      <select v-model ="equipe">
 			        <option>Escolha a equipe</option>
-			        <option>With options</option>
 			      </select>
 			    </div>
 			  </div>
@@ -48,48 +28,75 @@
 			  <label class="label">Permissões</label>
 			  <div class="control">
 			    <div class="select">
-			      <select>
+			      <select  v-model ="permissoes">
 			        <option>Visualizar</option>
 			        <option>Vizualizar e responder</option>
+			        <option>Editar</option>
 			      </select>
 			    </div>
 			  </div>
 			</div>
-	   </div>
-	   <div class="container is-widescreen">
-		  <div class="notification">
-			    <div class="field - column is-half">
-				  <label class="label">Pergunta</label>
-				  <div class="control">
-		    		<input class="input " type="text" placeholder="Pergunta">
-		          </div>
-				</div>
-				<div class="field column is-half is-horizontal">
-				  <div class="field-label is-normal">
-				    <label class="label">Opção:</label>
+			<div class = "field - colum is-half">
+			<div class="field-label column is-half" >
+			    <label class="label">Valores Invertidos?</label>
+			</div>
+			 <div class="field-body column is-half">
+			    <div class="field is-narrow">
+			      <div class="control">
+			        <input type="checkbox" v-model="valores_invertidos"> 
+			      </div>
+			    </div>
+			  </div>
+			 </div>
+			<div class="field column is-one-quarter">
+				  <div class="field-label is-normal column is-half">
+				    <label class="label">Valor Mínimo:</label>
 				  </div>
 				  <div class="field-body">
-				    <div class="field">
+				    <div class="field column is-half">
 				      <div class="control">
-				        <input class="input " type="text" placeholder="e.g. Partnership opportunity">
+				        <input class="input " type="text" placeholder="1" v-model ="valor_minimo">
+				      </div>
+				    </div>
+				  </div>
+		    </div>
+		    <div class="field column is-one-quarter">
+				  <div class="field-label is-normal column is-half">
+				    <label class="label">Valor Máximo:</label>
+				  </div>
+				  <div class="field-body">
+				    <div class="field column is-half">
+				      <div class="control">
+				        <input class="input " type="text" placeholder="10" v-model ="valor_maximo">
 				      </div>
 				    </div>
 				  </div>
 				</div>
+	   </div>
+	   <div class="container is-widescreen">
+		  <div class="notification">
+			    <div class="field - column is-half">
+				  <label class="label" id = "perg">{{perg_texto}}</label>
+				  <div class="control" >
+		    		<input class="input " type="text" placeholder="Pergunta" v-model ="pergunta">
+		          </div>
+				</div>
+				<div class="field is-horizontal">
+			  
+			</div>
 				
-				<div class="field column is-one-quarter is-pulled-right ">
-		            <div class="control is-pulled-right">
-		              <button class="button is-dark is-fullwidth ">Nova Opção</button>
-		            </div>
-	           </div>
+						
+	        <!--    <p>data: {{data}}</p> -->
          </div>
 	  </div>
 	  <div class="field column is-half is-pulled-right ">
 		            <div class="control is-pulled-right column is-half">
-		              <button class="button is-outlined is-fullwidth ">Nova Pergunta</button>
+		              <button class="button is-outlined is-fullwidth " v-on:click="novaPergunta()">Nova Pergunta</button>
 		            </div>
 		            <div class="control is-pulled-right column is-half">
-		              <button class="button is-dark is-fullwidth ">Finalizar Pergunta</button>
+		            <router-link :to="{name: 'ListarForms'}">
+		              <button class="button is-dark is-fullwidth "v-on:click="finalizarForm()" >Finalizar Formulário</button>
+		            </router-link>
 		            </div>
 	  </div>
 	</div>
@@ -97,7 +104,37 @@
 
 <script>
 export default {
-  name: 'criarForm'
+  name: 'criarForm',
+  data: () =>({
+  	nome_formulario: '',
+  	data_envio: '',
+  	permissoes: '',
+  	pergunta: '',
+  	valores_invertidos: '',
+  	valor_minimo: '',
+  	valor_maximo: '',
+  	equipe: '',
+  	perg_texto: 'Pergunta',
+  	count: 0,
+  	perguntas:[],
+  	novoForm: []
+     
+  }),
+  methods: {
+    novaPergunta() {
+       ++ this.count
+       this.perguntas.push(this.pergunta) 
+       this.perg_texto = 'Pergunta '+this.count
+
+    },
+    finalizarForm() {
+    	this.novoForm.push({nome:this.nome_formulario,data:this.data_envio,permissao:this.permissoes,pergunta:this.perguntas,valores_inv:this.valores_invertidos,valor_max:this.valor_maximo,valor_min:this.valor_minimo,eq:this.equipe})
+    	this.count = 0
+    }
+  }
 }
+
+
 </script>
-<style scoped></style>
+<style scoped>
+</style>
