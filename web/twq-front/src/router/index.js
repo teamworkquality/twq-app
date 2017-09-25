f2ee40693c7b89f7eb8d831b4115cad8f401b062
Bárbara Lima <Brbara Lima>
2017-09-25 09:53:55 -0300

    Mudança do router

--- web/twq-front/src/router/index.js
+++ web/twq-front/src/router/index.js
@@ -13,8 +13,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      name: 'Login',
+      component: Login
     },
     {
       path: '/login/',
