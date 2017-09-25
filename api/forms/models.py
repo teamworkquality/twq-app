f6ebd92eccdaa5d45d00fa2f4915dd07b1fb9021
fbormann <felipe.bormann@gmail.com>
2017-09-25 10:09:54 -0300

    feature(models): Add models for questions and forms

--- api/forms/models.py
+++ api/forms/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Form(models.Model):
+    name = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    has_time_limit = models.BooleanField(required=True)
+    time_limit = models.DateTimeField()
+
+class Question(models.Model):
+    text = models.TextField()
+    min = models.IntegerField()
+    max = models.IntegerField()
