2907d7b7df39245ea87e1551a84844a9450dbd02
fbormann <felipe.bormann@gmail.com>
2017-09-25 10:11:16 -0300

    feature(): add new views and urls

--- api/forms/views.py
+++ api/forms/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Form
+
+class FormsView(APIView):
+
+    queryset = Form.objects.all()
+
+    def get(self, request, format=None, **kwargs):
+        if kwargs.get('form_id'):
+            try:
+                return Response(None)
+            except ObjectDoesNotExist:
+                return Response({"error": "could not find user"}    , status=400)
+        else:
+            return Response(None)
