55443e1023565787cec8e8cd1cec15a9f341f6ae
fbormann <felipe.bormann@gmail.com>
2017-09-26 23:40:33 -0300

    refactor(): add all fiels to serializer handle

--- api/forms/serializers.py
+++ api/forms/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
-
 from forms.models import Form
 
-
-class Form(serializers.ModelSerializer):
+class FormSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Form
-        fields = ('id', 'name', 'is_admin', 'email')
-        read_only_fields = ('full_name', 'is_admin', 'email')
+        fields = '__all__'
