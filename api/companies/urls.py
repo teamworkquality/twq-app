820623bada86b5f0f823f3874d8c0888988767c3
fbormann <felipe.bormann@gmail.com>
2017-09-26 22:40:35 -0300

    feature(): fix companies urls path

--- api/companies/urls.py
+++ api/companies/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^(?P<company_id>\w+)$', include('forms.urls')),
+    url(r'^(?P<company_id>\w+)/forms/', include('forms.urls')),
 ]
