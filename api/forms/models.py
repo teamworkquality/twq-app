3d58de1acdb1fa5594007ab215ed39cd3131abf0
fbormann <felipe.bormann@gmail.com>
2017-09-26 23:39:29 -0300

    feature(): add null constraint on time_limit field

--- api/forms/models.py
+++ api/forms/models.py
@@ -4,7 +4,7 @@ class Form(models.Model):
     name = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     has_time_limit = models.BooleanField()
-    time_limit = models.DateTimeField()
+    time_limit = models.DateTimeField(null=True)
 
 class Question(models.Model):
     text = models.TextField()
