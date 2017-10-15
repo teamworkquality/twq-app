04e2bdcfd4cf1d6b785064db8304048a8834f342
Raquel Oliveira <rmso@cin.ufpe.br>
2017-10-15 19:44:00 -0300

    Envio de relatório - não funcional

--- web/twq-front/src/components/EnvioRelatorio.vue
+++ web/twq-front/src/components/EnvioRelatorio.vue
@@ -1,78 +1,41 @@
 <template>
 <div class = "container">
 		<div class="columns is-half">
-			<p class="is-size-2 has-text-weight-semibold">Envio Relatório</p>
+			<p class="is-size-2 has-text-weight-semibold">Envio de Relatório</p>
 		</div>
 		<div class="columns is-multiline is-mobile">
 			<div class="field - column is-one-quarter">
 			  <label class="label">Equipe</label>
 			  <div class="control"s>
 			    <div class="select">
-			      <select  v-model ="permissoes">
-			        <option>Visualizar</option>
-			        <option>Vizualizar e responder</option>
-			        <option>Editar</option>
+			      <select  v-model ="equipe">
+			        <option v-for = "e in equipesN">
+			        {{e.equip}}
+			        </option>
+			        
 			      </select>
 			    </div>
 			  </div>
 			</div>
-			<div class = "field - colum is-half">
-			<div class="field-label column is-half" >
-			    <label class="label">Valores Invertidos?</label>
-			</div>
-			 <div class="field-body column is-half">
-			    <div class="field is-narrow">
-			      <div class="control">
-			        <input type="checkbox" v-model="valores_invertidos"> 
-			      </div>
-			    </div>
-			  </div>
-			 </div>
-			<div class="field column is-one-quarter">
-				  <div class="field-label is-normal column is-half">
-				    <label class="label">Valor Mínimo:</label>
-				  </div>
-				  <div class="field-body">
-				    <div class="field column is-half">
-				      <div class="control">
-				        <input class="input " type="text" placeholder="1" v-model ="valor_minimo">
-				      </div>
-				    </div>
-				  </div>
-		    </div>
-		    <div class="field column is-one-quarter">
-				  <div class="field-label is-normal column is-half">
-				    <label class="label">Valor Máximo:</label>
-				  </div>
-				  <div class="field-body">
-				    <div class="field column is-half">
-				      <div class="control">
-				        <input class="input " type="text" placeholder="10" v-model ="valor_maximo">
-				      </div>
-				    </div>
-				  </div>
-				</div>
 	   </div>
 	   <div class="container is-widescreen">
 		  <div class="notification">
-			    <div class="field - column is-half">
-				  <label class="label" id = "perg">{{perg_texto}}</label>
-				  <div class="control" >
-		    		<input class="input " type="text" placeholder="Pergunta" v-model ="pergunta">
-		          </div>
-				</div>
-				<div class="field is-horizontal">
-			  
+			   
+			<div class="field">
+			  <div class="control">
+			    <label class="column is-half radio" v-for="n in nomes" >
+			      <input type="radio" name="question">
+			      {{n.nome}}
+			    </label>
+			  </div>
 			</div>
 				
-						
-	        <!--    <p>data: {{data}}</p> -->
          </div>
 	  </div>
 	  <div class="field column is-half is-pulled-right ">
 		            <div class="control is-pulled-right column is-half">
 		            <router-link :to="{name: 'ListarForms'}">
-		              <button class="button is-dark is-fullwidth "v-on:click="finalizarForm()" >Finalizar Formulário</button>
+		              <button class="button is-dark is-fullwidth "v-on:click="envioRelatorio()" >Enviar Relatório</button>
 		            </router-link>
 		            </div>
 	  </div>
@@ -80,6 +43,9 @@
 </template>
 
 <script>
+
+import HttpService from '@/service';
+
 export default {
   name: 'envioRelatorio',
   data: () =>({
@@ -94,17 +60,14 @@ export default {
   	perg_texto: 'Pergunta',
   	count: 0,
   	perguntas:[],
-  	novoForm: []
+  	novoForm: [],
+  	selected: 'ABC',
+  	nomes: [{nome: 'Renata'}, {nome: 'Bianca'}, {nome: 'Roberto'}],
+  	equipesN: [{equip: 'ABC'}, {equip: 'DEF'}, {equip: 'GHI'}]
      
   }),
   methods: {
-    novaPergunta() {
-       ++ this.count
-       this.perguntas.push(this.pergunta) 
-       this.perg_texto = 'Pergunta '+this.count
-
-    },
-    finalizarForm() {
+    envioRelatorio() {
     	this.novoForm.push({nome:this.nome_formulario,data:this.data_envio,permissao:this.permissoes,pergunta:this.perguntas,valores_inv:this.valores_invertidos,valor_max:this.valor_maximo,valor_min:this.valor_minimo,eq:this.equipe})
     	this.count = 0
     }
