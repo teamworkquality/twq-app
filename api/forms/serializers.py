b90cb41db13938e0b1183f4777f3e14c27df0b4e
fbormann <felipe.bormann@gmail.com>
2017-10-21 15:14:18 -0300

    feature(): add questions serializer and now the data for reading and writing is matching docs

--- api/forms/serializers.py
+++ api/forms/serializers.py
@@ -1,16 +1,23 @@
 from rest_framework import serializers
 from .models import Form, Question
 
-class FormSerializer(serializers.ModelSerializer):
-    questions = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = Form
-        fields = ('name', 'created_at', 'has_time_limit', 'time_limit', 'questions')
-
-
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = "_all_"
+        fields = ('text', 'min', 'max')
+
+class FormSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True)
+    time_limit = serializers.DateTimeField(required=False)
+
+    class Meta:
+        model = Form
+        fields = ('name', 'has_time_limit', 'time_limit', 'questions')
+
+    def create(self, validated_data):
+        questions_data = validated_data.pop('questions')
+        form = Form.objects.create(**validated_data)
+        for question_data in questions_data:
+            Question.objects.create(form=form, **question_data)
+        return form
