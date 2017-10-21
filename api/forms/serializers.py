8a7c13043001046043c9d84d6008f0c9900ece94
fbormann <felipe.bormann@gmail.com>
2017-10-21 14:10:21 -0300

    feature(): add questions on FormSerializer JSON and fix import way

--- api/forms/serializers.py
+++ api/forms/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from forms.models import Form
+from .models import Form, Question
 
 class FormSerializer(serializers.ModelSerializer):
+    questions = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Form
-        fields = '__all__'
+        fields = ('name', 'created_at', 'has_time_limit', 'time_limit', 'questions')
+
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = "_all_"
