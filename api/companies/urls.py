d45405d1c2b83ad19eaa4aa4195e7c7a913e23ad
fbormann <felipe.bormann@gmail.com>
2017-10-25 09:24:23 -0300

    refactor(views): fix package name and rename wrong import

--- api/companies/urls.py
+++ api/companies/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 
-from users.views import TeamView
+from .views import TeamView
 
 urlpatterns = [
     url(r'^$', TeamView.as_view(), name="teams"),
-    url(r'^(?P<team_id>\w+)$', UserView.as_view(), name="get_team"),
+    url(r'^(?P<team_id>\w+)$', TeamView.as_view(), name="get_team"),
 ]
