772ba24e8e1275a02e42bb14b9a85a3e6705048d
fbormann <felipe.bormann@gmail.com>
2017-10-25 21:36:12 -0300

    feature(urls): fix team urls

--- api/companies/urls.py
+++ api/companies/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import TeamView
 
 urlpatterns = [
-    url(r'^$', TeamView.as_view(), name="teams"),
-    url(r'^(?P<team_id>\w+)$', TeamView.as_view(), name="get_team"),
+    url(r'^/team$', TeamView.as_view(), name="teams"),
+    url(r'^team/(?P<team_id>\w+)$', TeamView.as_view(), name="get_team"),
 ]
