370bcf111d616e74159ac3ce93dda8e69307768c
fbormann <felipe.bormann@gmail.com>
2017-10-25 21:56:07 -0300

    feature(companies): add list all companies and create company

--- api/companies/views.py
+++ api/companies/views.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
 from .models import Company
 from .models import Team
@@ -44,3 +45,15 @@ class TeamView(APIView):
 
 class CompanyView(APIView):
     serializer_class = CompanySerializer
+
+    def get(self, request, format=None):
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
