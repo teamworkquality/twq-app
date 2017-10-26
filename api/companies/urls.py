126c25afe836d60214075a1f603d55e309c99ec8
fbormann <felipe.bormann@gmail.com>
2017-10-25 21:44:38 -0300

    feature(urls): add company_id as parameter for endpoint

--- api/companies/urls.py
+++ api/companies/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from .views import TeamView
 
 urlpatterns = [
-    url(r'^team/$', TeamView.as_view(), name="teams"),
-    url(r'^team/(?P<team_id>\w+)$', TeamView.as_view(), name="get_team"),
+    url(r'^(?P<company_id>\w+)/team/$', TeamView.as_view(), name="teams"),
+    url(r'^(?P<company_id>\w+)/team/(?P<team_id>\w+)$', TeamView.as_view(), name="get_team"),
     url(r'^(?P<company_id>\w+)/forms/', include('forms.urls')),
 ]
