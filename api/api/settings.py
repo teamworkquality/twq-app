38c18d52eaa680a83eafd6ebe0cc2ba3cb9c8302
fbormann <felipe.bormann@gmail.com>
2017-10-30 11:37:16 -0300

    feature(apps): reorder loading apps

--- api/api/settings.py
+++ api/api/settings.py
@@ -36,10 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
     'users',
     'companies',
+    'rest_framework',
+    'rest_framework.authtoken',
     'forms',
 ]
 
