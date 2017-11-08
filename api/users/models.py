b2f5f962ac6937c83a8d0b4cacca56327e430b0d
fbormann <felipe.bormann@gmail.com>
2017-11-08 10:40:57 -0300

    refactor(models): change inheniterance to User

--- api/users/models.py
+++ api/users/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import User
 
-class User(AbstractBaseUser):
+class User(User):
     full_name = models.CharField(max_length=250, blank=False)
-    email = models.EmailField(unique=True)
     is_admin = models.BooleanField(default=False, blank=False)
 
     USERNAME_FIELD = "email"
