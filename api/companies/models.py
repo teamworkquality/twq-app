464824a8a91a56caa2cdb127700b344bf81fb728
fbormann <felipe.bormann@gmail.com>
2017-11-14 23:02:37 -0300

    refactor(models): rename model from User to Admin

--- api/companies/models.py
+++ api/companies/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from users.models import User
+from users.models import Admin
 
 class Company(models.Model):
     name = models.CharField(max_length=250, unique=True)
-    owner = models.ForeignKey('users.User')
+    owner = models.ForeignKey('users.Admin')
     editors = models.ManyToManyField('users.User', related_name="editors")
 
 class Team(models.Model):
 	name = models.CharField(max_length=250, unique=True)
 
-class Employee(User):
+class Employee(Admin):
     employer = models.ForeignKey('companies.Company', related_name="employer")
     team = models.ForeignKey('companies.Team', null=True)
