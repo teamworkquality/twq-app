d61a4e1f19a1206f647e8a55639b73339524d4d6
fbormann <felipe.bormann@gmail.com>
2017-11-14 23:03:45 -0300

    refactor(users): rename User ocurrences to Admin

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import Admin
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = User
+        model = Admin
         fields = ('id', 'full_name', 'is_admin', 'email', 'username', 'password')
--- api/users/views.py
+++ api/users/views.py
@@ -1,26 +1,26 @@
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from users.models import User
+from users.models import Admin
 from users.serializers import UserSerializer
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.hashers import make_password
 
 class UserView(APIView):
 
-    queryset = User.objects.all()
+    queryset = Admin.objects.all()
     serializer_class = UserSerializer
 
     def get(self, request, format=None, **kwargs):
         if kwargs.get('user_id'):
             try:
-                user = User.objects.get(id=kwargs['user_id'])
+                user = Admin.objects.get(id=kwargs['user_id'])
                 user_data = UserSerializer(user)
                 return Response(user_data.data)
             except ObjectDoesNotExist:
                 return Response({"error": "could not find user"}    , status=400)
         else:
-            all_users = User.objects.all()
+            all_users = Admin.objects.all()
             all_users_serialized = UserSerializer(all_users, many=True)
             return Response(all_users_serialized.data)
 
@@ -44,7 +44,7 @@ class UserView(APIView):
     def delete(self, request, format=None, **kwargs):
         response = Response()
         if kwargs.get("user_id"):
-            user = User.objects.get(id=kwargs['user_id'])
+            user = Admin.objects.get(id=kwargs['user_id'])
             if user:
                 user.delete()
                 response.status_code = 204
