afc803b487bdf10d15eef45cebfdd898a5fbd035
amnv <amnv@cin.ufpe.br>
2017-11-26 01:00:34 -0300

    ajuste na url

--- api/companies/urls.py
+++ api/companies/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^(?P<company_id>\w+)/team/$', TeamView.as_view(), name="teams"),
     url(r'^(?P<company_id>\w+)/team/(?P<team_id>\w+)$', TeamView.as_view(), name="get_team"),
     url(r'^(?P<company_id>\w+)/employees/', EmployeeView.as_view(), name="get_employees_from_company"),
-url(r'^(?P<company_id>\w+)$', EmployeeView.as_view(), name="get_company"),
+    url(r'^(?P<company_id>\w+)$', CompanyView.as_view(), name="get_company"),
     url(r'^(?P<company_id>\w+)/forms/', include('forms.urls')),
     url(r'^$', CompanyView.as_view(), name="companies"),
 ]
